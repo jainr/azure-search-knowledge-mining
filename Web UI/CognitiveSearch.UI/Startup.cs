2125c501176510f61b3f19cbe36de37f4adbfff7
Pritish Devurkar <prdevurk@microsoft.com>
2020-05-06 12:05:39 -0400

    Revert commits 8681619

--- Web UI/CognitiveSearch.UI/Startup.cs
+++ Web UI/CognitiveSearch.UI/Startup.cs
@@ -25,7 +25,6 @@ namespace CognitiveSearch.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
